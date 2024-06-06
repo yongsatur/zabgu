db735a8a26f0fe85259d1fc2947571b7a181ab0f
alenagrigoryeva <alenagrigoryeva02@gmail.com>
2024-06-06 09:47:05 +0900

    Изменение способа отправки файлов на Google Drive

---
+++ plugins/form.js
@@ -0,0 +1,84 @@
+const sendArticles = document.querySelector('#sendArticles'), 
+    form = document.querySelector('#google_form_2');
+
+window.addEventListener('load', function () {
+    sendArticles.addEventListener('click', function(){
+        sendArticles.dataset.text = sendArticles.textContent;
+        var line = document.getElementById('line');
+        line.style.visibility = 'visible';
+        sendData();
+    })
+})
+
+function sendData() {
+    let data = new FormData(), uploadFiles = [], promiseList = [];
+    const articles = document.querySelector('#files'),
+        url = 'https://script.google.com/macros/s/AKfycbyZmpflwsYnxrEfTDLP9H95Vb7_jfcfxYDkj3dZIRZcnKd5zwPLr6Ptu1cij4n8x_pg/exec';
+    
+        form.querySelectorAll('input').forEach(input => {
+        if (input.type === "text" || input.type === "email") {
+            data.append(input.name, input.value)
+        }
+    })
+
+    for (let file of articles.files) {
+        const fr = new FileReader();
+        fr.readAsArrayBuffer(file);
+        let promise = new Promise((resolve) => {
+            fr.onload = f => {
+                let fileInfo = {
+                    fileArray: [...new Int8Array(f.target.result)],
+                    filename: file.name,
+                    mimeType: file.type,
+                }
+                resolve(uploadFiles.push(fileInfo))
+            }
+        })
+        promiseList.push(promise)
+    }
+    Promise.all(promiseList).then(result => {
+        data.append('files', JSON.stringify(uploadFiles))
+        fetch(`${url}`, { method: "POST", mode: 'no-cors', body: data })
+        .then(response => {
+            document.getElementById("nameFile").textContent = "Загрузить файл";
+            var line = document.getElementById('line');
+            line.style.visibility = 'hidden';
+            Swal.fire({
+                timer: 4000,
+                timerProgressBar: true,
+                confirmButtonColor: "#008d85",
+                confirmButtonText: "Отлично!",
+                focusConfirm: false,
+                text: 'Ваша статья была успешно отправлена!',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__fadeOutUp'
+                }
+            });
+            setTimeout(function () {
+                $('#google_form_2')[0].reset();
+            }, 1000);
+        })
+        .catch(err => {
+            Swal.fire({
+                timer: 4000,
+                timerProgressBar: true,
+                confirmButtonColor: "#008d85",
+                confirmButtonText: "Отлично!",
+                focusConfirm: false,
+                text: 'При загрузке файла произошла ошибка!',
+                showClass: {
+                    popup: 'animate__animated animate__fadeInDown'
+                },
+                hideClass: {
+                    popup: 'animate__animated animate__fadeOutUp'
+                }
+            });
+            setTimeout(function () {
+                $('#google_form_2')[0].reset();
+            }, 1000);
+        })
+    })
+}
--- plugins/plugins.js
+++ plugins/plugins.js
@@ -3,7 +3,6 @@
 new WOW().init();
 
 $(document).ready(function () {
-
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.scrollup').fadeIn();
@@ -15,7 +14,11 @@ $(document).ready(function () {
 		$("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
+});
 
+$('.file_upload input[type=file]').on('change', function(){
+	let file = this.files[0];
+	$(this).next().html(file.name);
 });
 
 $('#google_form_1').on('submit', function (e) {
@@ -37,40 +40,3 @@ $('#google_form_1').on('submit', function (e) {
 		$('#google_form_1')[0].reset();
 	}, 1000);
 });
-
-window.addEventListener('message', function(e) {
-	let message = e.data;
-
-	if (message == 'Загружен!') {
-		Swal.fire({
-			timer: 4000,
-			timerProgressBar: true,
-			confirmButtonColor: "#008d85",
-			confirmButtonText: "Отлично!",
-			focusConfirm: false,
-			text: 'Ваша статья была успешно отправлена!',
-			showClass: {
-				popup: 'animate__animated animate__fadeInDown'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__fadeOutUp'
-			}
-		});
-	}
-	else if (message == 'Ошибка!')  {
-		Swal.fire({
-			timer: 4000,
-			timerProgressBar: true,
-			confirmButtonColor: "#008d85",
-			confirmButtonText: "Отлично!",
-			focusConfirm: false,
-			text: 'При загрузке файла произошла ошибка!',
-			showClass: {
-				popup: 'animate__animated animate__fadeInDown'
-			},
-			hideClass: {
-				popup: 'animate__animated animate__fadeOutUp'
-			}
-		});
-	}
-} , false);
